4e453da02b8936f157b3e4a166d868d7d64b202f
Anydav <94948869+Anydav@users.noreply.github.com>
2025-07-15 13:05:16 +0100

    wiw

--- scripts/landingPage.js
+++
@@ -1,3 +0,0 @@
-const { createClient } = supabase;
-const supabase = createClient('https://upcreedrhrazbrbxgkyh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwY3JlZWRyaHJhemJyYnhna3loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMjEyNDAsImV4cCI6MjA2Nzg5NzI0MH0.PXxOutW_ex3uz_k69sCciEQsqP7AV9nUMl-ROR_AffM');
-
